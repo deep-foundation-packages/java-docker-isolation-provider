54f971aaa34459a2dfc5961603b92f20de8c4e25
Viacheslav <novikova835@gmail.com>
2023-09-18 15:37:38 +0300

    jep

--- src/main/java/org/provider/DeepClientPythonWrapper.java
+++ src/main/java/org/provider/DeepClientPythonWrapper.java
@@ -1,29 +1,77 @@
 package org.provider;
 
-import org.python.core.Py;
-import org.python.core.PyException;
-import org.python.core.PyObject;
-import org.python.util.PythonInterpreter;
+import jep.Jep;
+import jep.SharedInterpreter;
+import jep.Interpreter;
+import jep.JepException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DeepClientPythonWrapper {
-    private PythonInterpreter interpreter;
+    private final Interpreter jep;
 
-    public DeepClientPythonWrapper() {
-        interpreter = new PythonInterpreter();
+    public DeepClientPythonWrapper() throws JepException {
+        jep = new SharedInterpreter();
+        jep.exec("import sys\n" +
+                "sys.path.append('.')");
+        jep.exec("from deep_client_interface import *");
     }
 
-    public PyObject select(String token, String url, Object exp, PyObject options) {
-        try {
-            interpreter.exec("from deep_client_interface import select");
-            PyObject selectFunction = interpreter.get("select");
-            return selectFunction.__call__(Py.java2py(token), Py.java2py(url), Py.java2py(exp), options);
-        } catch (PyException e) {
-            e.printStackTrace();
-            return Py.None;
+    public void finalizePython() {
+        if (jep != null) {
+            jep.close();
         }
     }
 
+    public Map<String, Object> select(String token, String url, Object query) throws JepException {
+        Map<String, Object> result = new HashMap<>();
+        try {
+            jep.set("token", token);
+            jep.set("url", url);
+            jep.set("query", query);
+            jep.exec("result = select(token, url, query)");
+            result = jep.getValue("result", Map.class);
+        } catch (JepException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public Map<String, Object> insert(String token, String url, Object query) throws JepException {
+        Map<String, Object> result = new HashMap<>();
+        try {
+            jep.set("token", token);
+            jep.set("url", url);
+            jep.set("query", query);
+            jep.exec("result = insert(token, url, query)");
+            result = jep.getValue("result", Map.class);
+        } catch (JepException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
     public void close() {
-        interpreter.cleanup();
+        finalizePython();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Start: ");
+        try {
+            DeepClientPythonWrapper wrapper = new DeepClientPythonWrapper();
+
+           /* String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsiYWRtaW4iXSwieC1oYXN1cmEtZGVmYXVsdC1yb2xlIjoiYWRtaW4iLCJ4LWhhc3VyYS11c2VyLWlkIjoiMzgwIn0sImlhdCI6MTY5MTkxMTQxM30.W0GOuqOvRZrgrVZkLaceKTPBitXwR-1WlxLgxUZXOnY";
+            String url = "http://192.168.0.135:3006/gql";*/
+            /*Map<String, Object> query = new HashMap<>();
+            query.put("key", "value");*/
+
+            /*Map<String, Object> result = wrapper.select(token, url, 1);
+            System.out.println("Result: " + result);*/
+
+           // wrapper.close();
+            System.out.println("try: ");
+        } catch (JepException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/provider/Main.java
+++ src/main/java/org/provider/Main.java
@@ -1,20 +1,14 @@
 package org.provider;
 
-import org.python.core.PyObject;
-import org.python.core.Py;
-
 public class Main {
     public static void main(String[] args) {
         DeepClientPythonWrapper pythonWrapper = new DeepClientPythonWrapper();
 
-        PyObject result = pythonWrapper.select("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsiYWRtaW4iXSwieC1oYXN1cmEtZGVmYXVsdC1yb2xlIjoiYWRtaW4iLCJ4LWhhc3VyYS11c2VyLWlkIjoiMzgwIn0sImlhdCI6MTY5MTkxMTQxM30.W0GOuqOvRZrgrVZkLaceKTPBitXwR-1WlxLgxUZXOnY",
-                "http://192.168.0.135:3006/gql", 1, Py.None);
+        var result = pythonWrapper.select("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsiYWRtaW4iXSwieC1oYXN1cmEtZGVmYXVsdC1yb2xlIjoiYWRtaW4iLCJ4LWhhc3VyYS11c2VyLWlkIjoiMzgwIn0sImlhdCI6MTY5MTkxMTQxM30.W0GOuqOvRZrgrVZkLaceKTPBitXwR-1WlxLgxUZXOnY",
+                "http://192.168.0.135:3006/gql", 1);
 
-        if (result != Py.None) {
-            System.out.println("Result: " + result);
-        } else {
-            System.out.println("Error occurred.");
-        }
+        System.out.println("Result: " + result);
+
 
         pythonWrapper.close();
     }
